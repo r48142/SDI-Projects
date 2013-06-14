8b63cbc4aaa60a3cb5b2f5f7c75079bbaa882ef9
r48142 <djchyld@gmail.com>
2013-06-13 22:53:53 -0400

    Update roberts_dwain_assignment2.js

--- roberts_dwain_assignment2.js
+++ roberts_dwain_assignment2.js
@@ -1,1 +1,24 @@
-alert("JavaScript works!");
+// Author: Dwain Roberts
+// SDI 1306
+// June 13, 2013
+// Project 2 Story about my visit to my favorite casino 
+
+//Start
+
+
+
+//Variables
+
+
+var casinoname = "I need to win money at Maryland Live";
+var casinogames = ["Roulette", "Black Jack", "Poker"];
+var dollarslost = 300;
+var dollarsBet = 300;
+var dollarswon = 600;
+
+console.log("If I win money at Maryland Live then I will Play again.");
+console.log("I bet " + dollarsBet + "at the casino.");
+console.log("I won " + dollarswon + "at The casino.");
+
+
+
