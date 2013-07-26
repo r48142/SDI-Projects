63279c347ddf8bb6d964e920d30750397a1c9df6
r48142 <djchyld@gmail.com>
2013-07-25 22:58:07 -0400

    Update Roberts_Dwain_Assignment3.js
    final syntax fix

--- Roberts_Dwain_Assignment3.js
+++ Roberts_Dwain_Assignment3.js
@@ -1,6 +1,7 @@
-
-
-//The Company Basketball Tournament
+// Author: Dwain Roberts
+// SDI 1307
+// July 25, 2013
+// Project 3 The Company Basketball Tournament
 
 
 
@@ -15,13 +16,13 @@ var teamNames = {
         "wonGame": true,
         "rosterAmount":13
      
-      }
+      },
       {
         "Teamname": "Patriots",
         "wonGame": true,
         "rosterAmount": 10
      
-      }
+      },
       {
         "Teamname": "Bears",
         "wonGame": false,
@@ -31,43 +32,133 @@ var teamNames = {
 };
 
 //Global Variables
-var court = "Ehret Gym"
-var gameTime = "Morning"
-var 
+var court = "Ehret Gym";
+var gameTime = "Morning";
+var fullGym = true;
 
 
-//Method:Mutator
-function Team(name) {
-  var name_ = name;
- 
-  this.getName = function() {
-    return name_;
-  };
- 
-  this.setName = function(name) {
-    name_ = name;
-  };
+//Defined Objects
+var headCoachInfo = {
+
+// Number Property
+    numOfInjuries : 4,
+// Array Property
+    playersHurt : [" Ron Harris", " Jamal Collins", " Chris Thomas", " Johnathan Span"],
+// String Property
+    TrainerFirstName : "Carl",
+    TrainerLastName : "Woolard",
+    Coach : "Coach Terrance",
+
+
+
+//Mutator Method
+concessions : ["pizza", "french fries"]};
+concessions.push("chips", "soda");
+
+
+
+
+//Function Method
+    
+var addPlayersToRoster = function(myCoach, rosterSpace){
+var myCoach = headCoachInfo.coach;
+var rosterSpace = rosterAmount - numOfInjuries;
+
+if(mycoach == "Coach Terrance"){
+console.log("We have space on our team.");
+}else if(mycoach != "Coach Terrance"){
+console.log("There is no space on this team.");
+
+
 }
 
 
-//Method:Function
+};
 
-//Method:Boolean
 
-//Method:Accessor
 
-//Method:Procedure
+//Boolean Method
+var areWinningGame = function(scoreIsGood,holdTheball) {
+if  (scoreIsGood === true || (scoreIsGood && holdTheball === true) )
+{
+console.log("We are winning the game at" + court + ", so don't rush to score.");
+}
+else
+{
+  console.log ("We are losing the game, so lets push the tempo.");
+  }
+  return holdTheBall;
+};
 
-//Method:Object
-player=new Object();
-player.firstname="Dwain";
-player.lastname="Roberts";
-player.age=25;
-player.jerseycolor="red"; 
 
-//Returned Values
+
+//Accessor Method 
+//Get our team trainers first and last name.
+
+	getheadCoachInfo = function(){
+return headCoachInfo.trainerFirstName + " " + headCoachInfo.trainerLastName;
+
+
+};
+
+
+//Procedure Method
+	var recentScorer = function(minutesSinceScore){
+
+if(minutesSinceScore >= 5){
+console.log("You have to sit on the bench for a few minutes!");
+}else{
+
+console.log("Stay in the game because you just scored in the within the last 5 minutes!");
+return minutesSinceScore;
+	
+}
+
+//Return Number
+	playerToSub = function(subAtDesk){
+var playersToSit = 3;
+
+while(subAtDesk > 0){
+console.log("There is a " + subAtDesk +" waiting to come in. We have "+ playersToSit +" players that need to come to the bench.");
+custInLine--;
+
+for( x = 0; x < playersToSit ; x++ ){
+subAtDesk--;
+
+}
+
+}
+return subAtDesk;
+};
+
+
+//Object Method
+
+player = new Object();
+firstName=["Dwain"];
+lastname=["Roberts"];
+Age=25;
+jerseyColor=["red"]; 
+
+getplayer = function(){
+return player.firstName;
+
+
+
 
 //Output
+//Players Injured
+console.log("We have " + headcoachInfo.numOfInjuries + "injured players for this tournament.");
+//Food
+console.log("The staff at" + court + "now have" +concessions+ "on the menu.");
+//Rosters
+console.log("We have" + rosterSpace + "on our team due to injuries.");
+//Trainer 
+Console.log("Our trainers name is Carl Woolard.");
+//Finish
+}
+};
+
 
 //Finish
 
